749b67bf1a4a30f77831809836d71de1ec69c7e3
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-08-03 15:42:57 +0100

    styling on serach bar

--- tv_project/src/components/SearchBar.js
+++ tv_project/src/components/SearchBar.js
@@ -13,8 +13,17 @@ const SearchBar = () => {
 
 	return (
 		<div className='searchbar'>
-      <h4>Find a show</h4>
-			<input type='text' placeholder='Search shows...' value={searchQuery} onChange={handleInputChange} />
+			<div className="search">
+				<h4>Find a show</h4>
+				<input type='text' placeholder='Search shows...' value={searchQuery} onChange={handleInputChange} />
+			</div>
+			<div className="dropdown">
+				<h4>Sort by</h4>
+				<select>
+					<option value='default'>None</option>
+					<option value='name_ascending'>Show name ascending</option>
+				</select>
+			</div>
 		</div>
 	);
 };
