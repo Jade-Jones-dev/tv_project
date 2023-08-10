e22257fd615b14759daf25dacf6775779b16b138
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-08-10 19:09:59 +0100

    card styling

--- tv_project/src/components/Cards.js
+++ tv_project/src/components/Cards.js
@@ -13,12 +13,13 @@ const Cards = () => {
         <img src={show.image.medium} alt={show.name} />
         <div className='card-content'>
           <h5>
-            {show.name} - {show.runtime} mins
+            {show.name} 
           </h5>
-          <h5>Premiered on {show.premiered}</h5>
+          <h5>Show length {show.runtime} mins</h5>
+          <button onClick={() => setSelectedShow(show)}>Show details</button>
         </div>
         <div>
-          <button onClick={() => setSelectedShow(show)}>Show details</button>
+         
         </div>
       </div>
     );
